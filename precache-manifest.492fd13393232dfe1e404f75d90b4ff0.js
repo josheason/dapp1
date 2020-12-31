92bd8a5bbedb8e52dd252b99e6f627537da1dc3c
Josh Eason <josheason@Joshs-MacBook-Pro.local>
2020-12-31 11:35:19 -0500

    Updates

---
+++ precache-manifest.492fd13393232dfe1e404f75d90b4ff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f5d4e596244ebf24f24c66cccbaccd",
+    "url": "/dapp1/index.html"
+  },
+  {
+    "revision": "7d1594f835929845e265",
+    "url": "/dapp1/static/css/main.d39e5649.chunk.css"
+  },
+  {
+    "revision": "7589b10d89cb83f46091",
+    "url": "/dapp1/static/js/2.495afc00.chunk.js"
+  },
+  {
+    "revision": "7d1594f835929845e265",
+    "url": "/dapp1/static/js/main.402b921a.chunk.js"
+  },
+  {
+    "revision": "1cc9fbbe9076bbad6057",
+    "url": "/dapp1/static/js/runtime-main.b30e29ac.js"
+  },
+  {
+    "revision": "d75a301aace970c3cba414d0609079e9",
+    "url": "/dapp1/static/media/connected1.d75a301a.png"
+  },
+  {
+    "revision": "091c1c215c87a2bee495da56a643ba2c",
+    "url": "/dapp1/static/media/eth123.091c1c21.png"
+  }
+]);
